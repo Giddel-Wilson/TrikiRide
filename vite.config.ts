85c42c02ba8a2b9f83e6463000d254c2496eef44
Giddel-Wilson <you@example.com>
2026-07-12 23:34:42 +0100

    Fix vite.config.ts to use Netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -11,9 +11,7 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter({
-				runtime: 'nodejs22.x'
-			})
+			adapter: adapter()
 		})
 	]
 });
